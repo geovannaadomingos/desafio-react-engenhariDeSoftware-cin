48b7635f399429048ade7cc1e5a16d507284d100
Geovanna Domingos <gmdomingosn@gmail.com>
2023-07-25 22:33:08 -0300

    remove comentários e altera variáveis e funções

--- components/PokemonDetails.jsx
+++ components/PokemonDetails.jsx
@@ -1,84 +1,83 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './PokemonDetails.css';
+import { useState } from 'react'
+import axios from 'axios'
+import './PokemonDetails.css'
 
 const PokemonDetails = () => {
-  const [pokemonNumber, setPokemonNumber] = useState('');
-  const [pokemonName, setPokemonName] = useState('');
-  const [pokemonType, setPokemonType] = useState('');
-  const [pokemonSprite, setPokemonSprite] = useState('');
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [showDetails, setShowDetails] = useState(false);
+  const [pokemonId, setPokemonId] = useState('')
+  const [pokemonName, setPokemonName] = useState('')
+  const [pokemonType, setPokemonType] = useState('')
+  const [pokemonImage, setPokemonImage] = useState('')
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [showDetails, setShowDetails] = useState(false)
 
-  const fetchPokemonDetails = async () => {
-    setLoading(true);
-    setShowDetails(false);
-    setError('');
+  const getPokemonDetails = async () => {
+    setLoading(true)
+    setShowDetails(false)
+    setError('')
 
-    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-    await delay(2000);
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+    await delay(2000)
 
     try {
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`);
-      const { name, types, sprites } = response.data;
-      setPokemonName(name);
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
+      const { name, types, sprites } = response.data
+      setPokemonName(name)
+      setPokemonImage(sprites.front_default)
 
       if (types && types.length > 0) {
-        setPokemonType(types[0].type.name);
+        setPokemonType(types[0].type.name)
       } else {
-        setPokemonType('Unknown Type');
+        setPokemonType('Tipo desconhecido')
       }
 
-      setLoading(false);
-      setShowDetails(true);
-      setPokemonSprite(sprites.front_default); // Assuming you want the front default sprite.
+      setLoading(false)
+      setShowDetails(true)
     } catch (error) {
-      console.error('Error fetching Pokémon details:', error);
-      setError('Pokémon não encontrado');
-      setPokemonName('');
-      setPokemonType('');
-      setPokemonSprite('');
-      setLoading(false);
+      setError('Não foi possível encontrar o Pokémon')
+      setPokemonName('')
+      setPokemonType('')
+      setPokemonImage('')
+      setLoading(false)
     }
-  };
+  }
 
   const handleInputChange = (event) => {
-    setPokemonNumber(event.target.value);
-  };
+    setPokemonId(event.target.value)
+  }
 
   const handleRefresh = () => {
-    setError('');
-    setLoading(false);
-    setShowDetails(false);
-  };
+    setError('')
+    setLoading(false)
+    setShowDetails(false)
+  }
 
   return (
     <div>
-      {showDetails ? (
+      {showDetails ?
         <>
           <p>Nome do Pokémon: {pokemonName}</p>
           <p>Tipo do Pokémon: {pokemonType}</p>
-          {pokemonSprite &&
-            <div class="div-img"><img src={pokemonSprite} alt={pokemonName} /></div>
+          {pokemonImage &&
+            <div class="div-img"><img src={pokemonImage} alt={pokemonName} /></div>
           }
           <button onClick={handleRefresh}>Atualizar</button>
         </>
-      ) : (
+        :
         <>
           <input
             type="number"
             placeholder="Digite o número do Pokémon"
-            value={pokemonNumber}
+            value={pokemonId}
             onChange={handleInputChange}
           />
-          <button onClick={fetchPokemonDetails}>Atualizar</button>
+          <button onClick={getPokemonDetails}>Atualizar</button>
         </>
-      )}
-      {loading && <p>Loading...</p>}
+      }
+      {loading && <p>Carregando...</p>}
       {error && <p>{error}</p>}
     </div>
-  );
-};
+  )
+}
 
-export default PokemonDetails;
+export default PokemonDetails
--- components/PokemonList.jsx
+++ components/PokemonList.jsx
@@ -1,84 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './PokemonList.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './PokemonList.css'
 
 const PokemonList = () => {
-  const [pokemonList, setPokemonList] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
-  const [currentPage, setCurrentPage] = useState(1);
-  const limitPerPage = 20;
+  const [pokemonList, setPokemonList] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+  const [currentPage, setCurrentPage] = useState(1)
+  const limitPerPage = 20
 
-  const fetchPokemonData = async (page) => {
+  const getPokemonData = async (page) => {
     try {
-      const offset = (page - 1) * limitPerPage;
+      const offset = (page - 1) * limitPerPage
       const response = await axios.get(
         `https://pokeapi.co/api/v2/pokemon/?limit=${limitPerPage}&offset=${offset}`
-      );
-      const { results } = response.data;
-      const names = results.map((pokemon) => pokemon.name);
+      )
+      const { results } = response.data
+      const names = results.map((pokemon) => pokemon.name)
 
-      return names;
+      return names
     } catch (error) {
-      console.error('Error fetching Pokémon list:', error);
-      throw new Error('Falha ao buscar a lista de Pokémon');
+      throw new Error('Falha ao buscar a lista de Pokémon')
     }
-  };
+  }
 
-  // Function to fetch more data when the user scrolls to the bottom of the list
   const handleScroll = () => {
     const isAtBottom =
       window.innerHeight + document.documentElement.scrollTop ===
-      document.documentElement.offsetHeight;
+      document.documentElement.offsetHeight
 
     if (isAtBottom && !loading) {
-      // Increase the current page to load more data
-      setCurrentPage((prevPage) => prevPage + 1);
-      setLoading(true);
+      setCurrentPage((prevPage) => prevPage + 1)
+      setLoading(true)
     }
-  };
+  }
 
   useEffect(() => {
-    // Fetch initial data only when the component mounts
     const loadInitialData = async () => {
       try {
-        const names = await fetchPokemonData(1);
-        setPokemonList(names);
-        setLoading(false);
+        const names = await getPokemonData(1)
+        setPokemonList(names)
+        setLoading(false)
       } catch (error) {
-        setError(error.message);
-        setLoading(false);
+        setError(error.message)
+        setLoading(false)
       }
-    };
+    }
 
-    loadInitialData();
-  }, []); // Empty dependency array to ensure this runs only once when the component mounts
+    loadInitialData()
+  }, [])
 
   useEffect(() => {
-    // Fetch more data when the current page changes
     const loadMoreData = async () => {
       try {
-        const names = await fetchPokemonData(currentPage);
-        setPokemonList((prevList) => [...prevList, ...names]);
-        setLoading(false);
+        const names = await getPokemonData(currentPage)
+        setPokemonList((prevList) => [...prevList, ...names])
+        setLoading(false)
       } catch (error) {
-        setError(error.message);
-        setLoading(false);
+        setError(error.message)
+        setLoading(false)
       }
-    };
+    }
 
     if (currentPage > 1) {
-      loadMoreData();
+      loadMoreData()
     }
-  }, [currentPage]);
+  }, [currentPage])
 
-  // Add scroll event listener when the component mounts
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [loading]); // Add 'loading' as a dependency to avoid redundant listeners
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [loading])
 
   return (
     <div>
@@ -87,13 +81,13 @@ const PokemonList = () => {
         {pokemonList.map((name, index) => (
           <li key={name}>
             {name}
-            {index === pokemonList.length - 1 && loading && <span> (Loading more...)</span>}
+            {index === pokemonList.length - 1 && loading && <span> (Carregando...)</span>}
           </li>
         ))}
       </ul>
       {error && <p>{error}</p>}
     </div>
-  );
-};
+  )
+}
 
-export default PokemonList;
+export default PokemonList
